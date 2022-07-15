d818815a0bbc8074943b79a0c478d443fd179a5f
naguedara <enranb@gmail.com>
2022-07-15 06:46:34 +0530

    cards demo project added

---
+++ CardDemo/src/test/java/com/example/CardDemo/CardDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CardDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
